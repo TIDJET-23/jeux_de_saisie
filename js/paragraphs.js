e21210a9675880a95833835437e628adb2dde5d7
khireddine tidjet <khireddinetidjet@gmail.com>
2022-11-28 19:40:24 +0000

    first commit

---
+++ js/paragraphs.js
@@ -0,0 +1,9 @@
+const paragraphs = [
+    "Certaines familles ont pour tradition de déposer des biscuits et un verre de lait près du sapin pour remercier le père noël.",
+	"Je n'ai qu'un jour pour visiter Paris. Je suis l'itinéraire donné par Pierre, un ami français. Je commence par un petit-déjeuner dans un café près de l'Arc de Triomphe avec des croissants délicieux. Après une marche sur les Champs-Élysées, je suis au pied de la Tour Eiffel. Il y a beaucoup d'attente pour les ascenseurs (pour monter les étages) mais je suis enfin au sommet et peux apprécier la vue sur Paris.",
+	"Comment vas-tu ? Je t’écris de France, où je vis avec ma famille depuis deux mois. Nous allons bien, nous habitons dans une petite maison ancienne avec un très beau jardin. Nous avons de nouveaux amis et nous avons rencontré nos voisins lors d’une soirée festive la semaine dernière.",
+	"Assis en terrasse dans le quartier touristique du Vieux Lyon, James sirote une limonade bien fraîche. Avec la chaleur estivale, cette journée d’exploration se révèle très fatigante !",
+	"Ce matin, James s’est levé tôt, a loué un vélo et a parcouru les rues de Lyon. Il a longé les quais de la Saône, traversé la presqu’île, puis rejoint les quais du Rhône. En chemin, il a pu admirer la magnifique fontaine Bartholdi sur la grande Place des Terreaux. Il s’est également arrêté dans une boulangerie où il a longuement hésité entre un pain au chocolat, un croissant, et une brioche aux pralines, pâtisserie typiquement lyonnaise. Il a finalement opté pour cette dernière."
+];
+
+//khireddine #22 @tidjet
---
+++ js/script.js
@@ -0,0 +1,85 @@
+const typingText = document.querySelector(".typing-text p"),
+inpField = document.querySelector(".wrapper .input-field"),
+tryAgainBtn = document.querySelector(".content button"),
+timeTag = document.querySelector(".time span b"),
+mistakeTag = document.querySelector(".mistake span"),
+cpmTag = document.querySelector(".cpm span");
+//khireddine #22 @tidjet
+
+let timer,
+maxTime = 120,
+timeLeft = maxTime,
+charIndex = mistakes = isTyping = 0;
+
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if(charIndex < characters.length - 1 && timeLeft > 0) {
+        if(!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if(typedChar == null) {
+            if(charIndex > 0) {
+                charIndex--;
+                if(characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if(characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer);
+        inpField.value = "";
+    }   
+}
+
+function initTimer() {
+    if(timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
